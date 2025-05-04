b48b040de171bac23b0964d400ecbf841a3742b2
Manya8819 <manyakumari.tit@gmail.com>
2025-05-04 23:17:23 +0530

    Create Task_2.py

---
+++ Task_2.py
@@ -0,0 +1,10 @@
+students = [
+    (101, "Alice", 20),
+    (102, "Bob", 21),
+    (103, "Charlie", 19)
+]
+
+print("\nStudent Information:")
+for student in students:
+    roll_number, name, age = student
+    print(f"Roll Number: {roll_number}, Name: {name}, Age: {age}")
